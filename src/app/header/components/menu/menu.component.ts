46deb35c3f7bb25bb19ff43fcd7f6d6188e9bd91
Matias Ringa <ringa.matias@gmail.com>
2021-10-01 15:29:28 +0200

    add fake menu

--- src/app/header/components/menu/menu.component.ts
+++ src/app/header/components/menu/menu.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  menuData = [{
+    text: 'Categoria 1',
+    link: 'categoria-1'
+  },{
+    text: 'Categoria 2',
+    link: 'categoria-2'
+  },{
+    text: 'Categoria 3',
+    link: 'categoria-3'
+  },{
+    text: 'Categoria 4',
+    link: 'categoria-4'
+  },];
 
   constructor() { }
 
