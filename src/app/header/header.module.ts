bf7080131aad190f3ce1ec510389c69409b5fd04
unknown <lemongnz@gmail.com>
2021-10-01 11:50:07 -0300

    second commit

---
+++ src/app/header/header.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LogoComponent } from './components/logo/logo.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HeaderLogOut } from './pages/layout/layout';
+
+
+@NgModule({
+  declarations: [
+    LogoComponent,
+    LoginComponent,
+    MenuComponent,
+    HeaderLogOut
+  ],
+  imports: [
+    CommonModule,
+    
+  ],
+  exports: [
+    LogoComponent,
+    LoginComponent,
+    MenuComponent,
+    HeaderLogOut
+  ]
+})
+export class HeaderModule { }
+ 
